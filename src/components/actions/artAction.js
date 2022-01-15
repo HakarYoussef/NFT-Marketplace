c99621a60baec0f36cfba3949ff424d4f090d1ca
Hakar <hakar.yusuf@gmail.com>
2022-01-15 19:34:51 +0300

    update

--- src/components/actions/artAction.js
+++ src/components/actions/artAction.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 export const loadArts = () => {
   return async (dispatch) => {
     try {
-      const artworks = await axios.get('/artworks.json');
+      const artworks = await axios.get(
+        'https://nft-marketplace-77bfd-default-rtdb.europe-west1.firebasedatabase.app/'
+      );
       return dispatch(setArts(artworks.data));
     } catch (error) {
       console.log('error:', error);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/store/store';
 
 ReactDOM.render(
-  <HashRouter basename="/NFT-Marketplace1">
+  <HashRouter basename="/">
     <Provider store={store}>
       <App />
     </Provider>
